e804833c58537778f4ed6683065020f3ebc59d2c
gandres <gandres.digital@gmail.com>
2024-03-15 13:33:52 +0100

    fix type

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -36,7 +36,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(event) => {
           handleSearch(event.target.value);
         }}
-        defaultValue={params.get('query')}
+        defaultValue={searchParams.get('query')?.toString()}
+
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
